3cbe326d90910e91ada9d8383fa49b21bc4da2ea
Sarah <sarah.dalsanto@tecnicosuperiorekennedy.it>
2018-10-16 02:07:56 -0700

    login

---
+++ GruppoC16-10/Login/LoginController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginController.swift
+//  GruppoC16-10
+//
+//  Created by Sarah Dal Santo on 16/10/18.
+//  Copyright © 2018 developer.llb. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
