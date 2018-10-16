49173a90cee5ed8db53dec4ba1a0ff1d4b91a80a
ShadowFIre97 <llb.developer.97@gmail.com>
2018-10-16 09:32:42 +0200

    Initial Commit

---
+++ GruppoC16-10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GruppoC16-10
+//
+//  Created by Leo Luca Bonin on 16/10/2018.
+//  Copyright © 2018 developer.llb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
