e847115009d3e38fa4b1da5657b3f66cd89fdece
Leonardo De Benetti <leonardo.debe@yahoo.it>
2018-10-16 03:02:18 -0700

    Storyboard e classi

---
+++ GruppoC16-10/FilmDetails/FilmDetailsCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilmDetailsCell.swift
+//  GruppoC16-10
+//
+//  Created by Leonardo De Benetti on 16/10/2018.
+//  Copyright © 2018 developer.llb. All rights reserved.
+//
+
+import UIKit
+
+class FilmDetailsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
